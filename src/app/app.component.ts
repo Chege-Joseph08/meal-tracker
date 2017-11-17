d6471c6eca3395a78559ca718e6440fc595449fb
JOSEPH CHEGE <josephchege08@gmail.com>
2017-11-17 17:54:58 +0300

    modified:   src/app/app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Meal } from './meal.model';
+import { MealListComponent } from './meal-list.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  //directives: [MealListComponent],
+  template: `
+    <div class="container">
+      <h1>Meal Tracker</h1>
+      <meal-list
+        [mealList]="meals">
+      </meal-list>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  public meals: Meal[];
+  constructor(){
+    this.meals = [
+      new Meal("Vegan Burger", "Ordered a salad on the side instead of fries.", 375),
+      new Meal("Falafel", "Didn't order feta cheese.", 275),
+      new Meal("Whole Bowl", "I skipped the sour cream and cheese.", 450)
+    ];
+  }
 }
