023d77e4d5ea5b8c67078f6f62bddfa389449018
JOSEPH CHEGE <josephchege08@gmail.com>
2017-11-17 18:32:42 +0300

    Modified edit-meal-details

--- src/app/edit-meal-details.component.ts
+++ src/app/edit-meal-details.component.ts
@@ -1,28 +1,59 @@
-import { Component, EventEmitter } from '@angular/core';
-import { Meal } from './meal.model';
+import { Component, Input, Output, EventEmitter} from '@angular/core';
+import { Meal } from './meal.model'
 
 @Component({
-  selector: 'edit-meal-details',
-  inputs: ['meal'],
-  outputs: ['onUpdateTotalCalories'],
+  selector: 'edit-meal',
   template: `
-    <div class="edit-form">
-      <h3>Edit Description:</h3>
-      <input [(ngModel)]="meal.name"/>
-      <input [(ngModel)]="meal.details"/>
-      <input [(ngModel)]="meal.calories" type="number" #newCalories/>
-      <button (click)="updateTotalCalories(newCalories)">Update</button>
+<!--If meal is selected, enter its details in the input boxes to edit-->
+<div *ngIf="childselectedMeal">
+
+  <!-- Modal -->
+<div id="editModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Edit Meal</h4>
+      </div>
+      <div class="modal-body">
+      <div>
+        <span class="label label-warning"><label>Enter Meal Name</label></span>
+        <input class="form-control" [(ngModel)]="childselectedMeal.mealName"><br><br>
+       </div>
+       <div>
+           <span class="label label-info"><label>Enter Meal Description</label></span>
+           <input class="form-control" [(ngModel)]="childselectedMeal.Description"><br><br>
+       </div>
+
+      <div>
+        <span class="label label-danger"><label>Enter Calories</label></span>
+          <input class="form-control" [(ngModel)]="childselectedMeal.calories">
+
+      </div>
+
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-default" data-dismiss="modal" (click)="doneClicked()">Done</button>
+      </div>
     </div>
+
+  </div>
+</div>
+
+  </div>
+
+
   `
+
 })
-export class EditMealDetailsComponent {
-  public meal: Meal;
-  public onUpdateTotalCalories: EventEmitter<number>;
-  constructor() {
-    this.onUpdateTotalCalories = new EventEmitter();
+export class EditMealComponent {
+
+  @Input() childselectedMeal: Meal;
+  @Output() doneClickedSender = new EventEmitter();
+  doneClicked() {
+    this.doneClickedSender.emit();
   }
-  updateTotalCalories(inputCalories: HTMLInputElement): void {
-    var newMealCalories: number = parseInt(inputCalories.value);
-    this.onUpdateTotalCalories.emit(newMealCalories);
-  }
+
 }
